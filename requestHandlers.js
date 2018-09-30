d313d8d7aad5abba1b141066969b7d0c1bffa294
Ryan Luras <ryanluras@gmail.com>
2018-09-30 16:48:10 -0700

    upload working

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,9 @@
-/* eslint-disable no-console, no-unused-vars, no-undef*/
+/* eslint-disable no-console */
 const querystring = require('querystring');
 const fs = require('fs');
+const formidable = require('formidable');
 
-function start(response, postData) {
+function start(response) {
     console.log('Request handler "start" was called.');
 
     let body = '<html>' +
@@ -12,7 +13,7 @@ function start(response, postData) {
         '</head>' +
         '<body>' +
         '<form action="/upload" enctype="multipart/form-data" method="post">' +
-        '<input type="file" name="upload">' +
+        '<input type="file" name="upload" multiple="multiple">' +
         '<input type="submit" value="Upload file" />' +
         '</form>' +
         '</body>' +
@@ -21,20 +22,33 @@ function start(response, postData) {
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(body);
     response.end();
-
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log('Request handler "upload" was called.');
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('You\'ve sent ' + querystring.parse(postData).text);
-    response.end();
+
+    let form = new formidable.IncomingForm();
+    console.log('about to parse');
+    form.parse(request, function(error, fields, files) {
+        console.log('parsing done');
+        
+        fs.rename(files.upload.path, 'tmp/test.png', function(error) {
+            if(error) {
+                fs.unlink('tmp/test.png');
+                fs.rename(files.upload.path, 'tmp/test.png');
+            }
+        });
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write('recieved image:<br/>');
+        response.write('<img src="/show" />');
+        response.end();
+    });
 }
 
 function show(response) {
     console.log('Request handler "show" was called.');
     response.writeHead(200, { 'Content-Type': 'image/png' });
-    fs.createReadStream('temp/test.png').pipe(response);
+    fs.createReadStream('tmp/test.png').pipe(response);
 }
 
 module.exports = {
